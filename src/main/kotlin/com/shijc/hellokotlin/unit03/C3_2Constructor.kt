802b7a88fd85e4065deca80aec1f2e17b5ff7507
shijiacheng <iot_shijiacheng@163.com>
2018-04-21 10:28:41 +0800

    构造器

--- src/main/kotlin/com/shijc/hellokotlin/unit03/C3_2Constructor.kt
+++ src/main/kotlin/com/shijc/hellokotlin/unit03/C3_2Constructor.kt
@@ -6,53 +6,71 @@ package com.shijc.hellokotlin.unit03
 
 }*/
 
-class Person (firstName:String){
+class Person(firstName: String) {
     var name = firstName
+
     init {
         println(firstName)
     }
 }
 
 
-class QACommunity(var url:String){
+class QACommunity(var url: String) {
     //主构造器实现
     init {
         println(url)
     }
 
     // 第二构造器（通过this直接调用了主构造器）
-    constructor(value:Int):this("shijiacheng.studio"){
+    constructor(value: Int) : this("shijiacheng.studio") {
         println(value)
     }
 
     // 第二构造器（通过this直接调用了主构造器）
-    constructor(desc:String,url: String):this("("+url+")"){
-        println(desc+":"+url)
+    constructor(desc: String, url: String) : this("(" + url + ")") {
+        println(desc + ":" + url)
     }
 
     // 第二构造器（通过this调用了第二构造器，间接的调用了主构造器）
-    constructor():this(20){
+    constructor() : this(20) {
         println("<http://shijiacheng.studio>")
     }
 }
 
+
+/*单例模式*/
+class Singleton private constructor() {
+    public var value: Singleton? = null
+
+    private object mHolder {
+        val INSTANCE = Singleton()
+    }
+
+    companion object Factory {
+        fun getInstance(): Singleton {
+            return mHolder.INSTANCE
+        }
+    }
+
+}
+
+class Customer(var customerName:String = "Bill",var value: Float = 20.4F){
+
+}
+
 fun main(args: Array<String>) {
-    println("*****第1个*****")
+    /*println("*****第1个*****")
     QACommunity("http://shijiacheng.studio")
     println("*****第2个*****")
     QACommunity(20)
     println("*****第3个*****")
-    QACommunity("我的博客","http://shijiacheng.studio")
+    QACommunity("我的博客", "http://shijiacheng.studio")
     println("*****第4个*****")
-    QACommunity()
-}
+    QACommunity()*/
 
-class Singleton private constructor(){
-    public var value:Singleton? = null
-    private object mHolder{
-        val INSTANCE = Singleton()
+    var obj1 = Singleton.getInstance()
+    var obj2 = Singleton.getInstance()
 
-        co
-    }
-
+    println(obj1)
+    println(obj2)
 }
